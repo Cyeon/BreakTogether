05a81e70f6c786caae84cc6fb1bcccbc14879175
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-12-12 15:24:34 +0900

    내장 회복

--- BreakTogether/Bullet.cpp
+++ BreakTogether/Bullet.cpp
@@ -7,8 +7,7 @@
 #include "Collider.h"
 
 Bullet::Bullet()
-	: m_fTheta(3.f * M_PI / 2.f)
-	  , m_vDir(Vec2(1.f, 1.f))
+	: m_vDir(Vec2(1.f, 1.f))
 {
 	m_pImage = ResMgr::GetInst()->ImgLoad(L"BulletImg", L"Image\\Bullet.bmp");
 	m_vDir.Normalize();
@@ -24,10 +23,6 @@ Bullet::~Bullet()
 void Bullet::Update()
 {
 	Vec2 vPos = GetPos();
-	//vPos.x += 700.f * fDT; 
-	//vPos.y += 700.f * fDT * m_fDir;
-	//vPos.x += 700.f * cosf(m_fTheta) * fDT;
-	//vPos.y -= 700.f * sinf(m_fTheta) * fDT;
 	vPos.x += 700.f * m_vDir.x * fDT;
 	vPos.y += 700.f * m_vDir.y * fDT;
 	SetPos(vPos);
@@ -35,25 +30,17 @@ void Bullet::Update()
 
 void Bullet::Render(HDC _dc)
 {
-	//Vec2 vPos = GetPos();
-	//Vec2 vScale = GetScale();
-	//Ellipse(_dc,
-	//	  (int)(vPos.x - vScale.x / 2.f)
-	//	, (int)(vPos.y - vScale.y / 2.f)
-	//	, (int)(vPos.x + vScale.y / 2.f)
-	//	, (int)(vPos.y + vScale.y / 2.f));
 	int Width = static_cast<int>(m_pImage->GetWidth());
 	int Height = static_cast<int>(m_pImage->GetHeight());
 
 	Vec2 vPos = GetPos();
-	//¸¶Á¨Å¸ »ö»ó »¬¶§ transparent: Åõ¸íÇÑ
 	TransparentBlt(_dc
-	               , static_cast<int>(vPos.x - (float)(Width / 2))
-	               , static_cast<int>(vPos.y - (float)(Height / 2))
-	               , Width, Height
-	               , m_pImage->GetDC()
-	               , 0, 0, Width, Height
-	               , RGB(255, 0, 255));
+		, static_cast<int>(vPos.x - (float)(Width / 2))
+		, static_cast<int>(vPos.y - (float)(Height / 2))
+		, Width, Height
+		, m_pImage->GetDC()
+		, 0, 0, Width, Height
+		, RGB(255, 0, 255));
 	Component_Render(_dc);
 }
 
